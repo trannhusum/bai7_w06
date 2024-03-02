026f7ea6a1bc752f9107ef3661a4a83d8d2c5c7c
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-02 15:57:20 +0700

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace bai7_w06
+{
+    public class Circle
+    {
+        private string color;
+        private double radius;
+        public Circle()
+        {
+            color = "";
+            radius = 0;
+        }
+        public Circle(string color, double radius)
+        {
+            this.color = color;
+            this.radius = radius;
+        }
+        public double getArea() 
+        {
+            return Math.Round(radius * radius*Math.PI);
+        }
+        public override string ToString()
+        {
+            return "Radius:" + getRadius() +", Color:" + getColor();
+        }
+        public double getRadius() 
+        {
+            return radius;
+        }
+        public void setRadius(double radius) 
+        {
+            this.radius=radius;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+    }
+    public class Cylinder : Circle
+    {
+        private double height;
+
+        public Cylinder()
+        {
+            height = 0;
+        }
+
+        public Cylinder(double height, string color, double radius) : base(color, radius)
+        {
+            this.height = height;
+        }
+        public double getVolume()
+        {
+            return Math.Round(getRadius() * getRadius() * Math.PI * height);
+        }
+
+        public override string ToString()
+        {
+            return "Radius:" + getRadius() + ", Height:" + height + ", Color:" + getColor();
+        }
+        public void setHeight(double height)
+        {
+            this.height=height;
+        }
+        public double getHeight()
+        {
+            return height;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai7_w06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle hinhtron= new Circle("lam",10);
+            Console.WriteLine(hinhtron);
+            Console.WriteLine("Dien tich cua hinh tru mau " + hinhtron.getColor() + ", banh kinh " + hinhtron.getRadius() + " la:" + hinhtron.getArea());
+            Cylinder hinhtru =new Cylinder(5,"xanh",5);
+            Console.WriteLine(hinhtru);
+            Console.WriteLine("The tich cua hinh tru mau "+hinhtru.getColor()+", chieu cao "+hinhtru.getHeight()+", banh kinh "+hinhtru.getRadius()+" la:"+hinhtru.getVolume());
+            Console.ReadKey();
+        }
+    }
+}
